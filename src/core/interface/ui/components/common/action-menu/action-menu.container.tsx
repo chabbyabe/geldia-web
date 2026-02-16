43ec0596bcb88180038e18587d11fd2dd39e5ba5
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 21:48:55 +0100

    Create action menu UI - A tooltip that shows edit/delete button

---
+++ src/core/interface/ui/components/common/action-menu/action-menu.container.tsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from 'react'
+import type { MouseEvent } from "react";
+import ActionMenuView from '@interface/ui/components/common/action-menu/action-menu.view';
+import AccountsController from '@interface/ui/screens/accounts/accounts.controller';
+
+export interface IActionMenuContainerModel {
+  children?: React.ReactNode
+  handleEditModal: (value: boolean) => void
+  handleDeleteModal: (value: boolean) => void
+  handleModalAction: (value: number) => void
+  actionId: number
+}
+
+export const ActionMenuContainer: React.FC<IActionMenuContainerModel> = (props) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleOpen = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const controller = new AccountsController();
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    controller.removeCurrentAccount();
+  };
+
+  const openEditModal = useCallback(async () => {
+    await props.handleModalAction(props.actionId);
+    props.handleEditModal(true);
+  }, []);
+
+  const openDeleteModal = useCallback(async() => {
+    await props.handleModalAction(props.actionId);
+    props.handleDeleteModal(true);
+  }, []);
+
+  return <ActionMenuView
+    children={props.children}
+    handleOpen={handleOpen}
+    handleClose={handleClose}
+    handleEditModal={openEditModal}
+    handleDeleteModal={openDeleteModal}
+    open={open}
+    anchorEl={anchorEl ?? undefined} 
+  />
+}
---
+++ src/core/interface/ui/components/common/action-menu/action-menu.view.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import type { MouseEvent } from "react";
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+
+export interface IActionMenuViewModel {
+  children?: React.ReactNode
+    handleOpen? : (event: MouseEvent<HTMLElement>) => void
+    handleClose? : () => void
+    handleEditModal: () => void
+    handleDeleteModal: () => void
+    open? : boolean | false
+    anchorEl? : HTMLElement | null
+}
+
+const ActionMenuView: React.FC<IActionMenuViewModel> = (props) => {
+  return (
+    <React.Fragment>
+      <IconButton size="small" onClick={props.handleOpen}>
+        <MoreVertIcon fontSize="small" />
+      </IconButton>
+
+      <Menu
+        anchorEl={props.anchorEl}
+        open={props.open ?? false}
+        onClose={props.handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={props.handleEditModal}>
+          <ListItemIcon>
+            <EditIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Edit</ListItemText>
+        </MenuItem>
+
+        <MenuItem onClick={props.handleDeleteModal} sx={{ color: "error.main" }}>
+          <ListItemIcon>
+            <DeleteIcon fontSize="small" color="error" />
+          </ListItemIcon>
+          <ListItemText>Delete</ListItemText>
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+};
+
+export default ActionMenuView;
