09bfa65398dbd05761c22a24592d5d0ad2304979
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 21:47:41 +0100

    Create delete account modal UI

--- src/core/interface/ui/components/modals/delete-confirmation-modal/delete-confirmation-modal.container.tsx
+++ src/core/interface/ui/components/modals/delete-confirmation-modal/delete-confirmation-modal.container.tsx
@@ -1,38 +1,28 @@
 import React from "react";
-import DeleteConfirmationModalView from "./delete-confirmation-modal.view";
-import * as Yup from "yup";
+import DeleteConfirmationModalView, { TInitialValues } from "./delete-confirmation-modal.view";
+import pluralize from "pluralize";
 
 interface IDeleteConfirmationModal {
-  open: boolean;
-  itemName: string;
-  onClose: () => void;
-  onConfirm: () => Promise<void> | void;
+  open: boolean
+  pageTitle: string
+  hasConfirmation: boolean
+  onClose: () => void
+  onConfirm: () => Promise<void> | void
 }
-
-interface FormValues {
-  confirmation: string;
-}
-
-const validationSchema = Yup.object({
-  confirmation: Yup.string()
-    .oneOf(["DELETE"], "You must type DELETE to confirm")
-    .required("Required"),
-});
-
 
 const DeleteConfirmationModal: React.FC<IDeleteConfirmationModal> = (props) => {
-  const initialValues: FormValues = {
+  const initialValues : TInitialValues = { 
+    hasConfirmation: props.hasConfirmation, 
+    pageTitle: pluralize.singular(props.pageTitle),
     confirmation: "",
   };
-
   return (
     <DeleteConfirmationModalView
       open={props.open}
-      itemName={props.itemName}
+      pageTitle={props.pageTitle}
       initialValues={initialValues}
       onClose={props.onClose}
       onConfirm={props.onConfirm}
-      validationSchema={validationSchema}
     />
   );
 };
