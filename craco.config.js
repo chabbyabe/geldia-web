a666f1339267a8449f7f2824d89ce8c9af0958f9
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 16:50:50 +0100

    Update dependency to cater testcases - Currently we are using Craco for file declaration and because of that there are problems with testing. CRA (Create React App)  uses Jest for testing so we need to update our craco file to cater jest.

--- craco.config.js
+++ craco.config.js
@@ -10,5 +10,18 @@ module.exports = {
       "@interface": path.resolve(__dirname, "src/core/interface"),
       "@screens": path.resolve(__dirname, "src/core/interface/ui/screens"),
     }
-  }
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        "^@base/(.*)$": "<rootDir>/src/$1",
+        "^@core/(.*)$": "<rootDir>/src/core/$1",
+        "^@data/(.*)$": "<rootDir>/src/core/data/$1",
+        "^@domain/(.*)$": "<rootDir>/src/core/domain/$1",
+        "^@interface/(.*)$": "<rootDir>/src/core/interface/$1",
+        "^@screens/(.*)$":
+          "<rootDir>/src/core/interface/ui/screens/$1",
+      },
+    },
+  },
 };
