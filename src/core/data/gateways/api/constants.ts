2a21d461841b2e8e4cf0d810573f2e021ec2d038
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 21:02:34 +0100

    Add api url in constants.ts - Add account url - Add user url

--- src/core/data/gateways/api/constants.ts
+++ src/core/data/gateways/api/constants.ts
@@ -4,3 +4,9 @@
 export const LOGIN_URL = `/users/auth/login/`
 export const LOGOUT_URL = `/users/auth/logout/`
 export const REGISTER_URL = `/users/auth/register`
+
+/**
+ *  URLS
+ */
+export const ACCOUNT_URL = `/ledger/accounts/`
+export const USER_URL = `/ledger/users/`
