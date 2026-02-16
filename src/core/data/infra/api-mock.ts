1071e8ddb7e4db8ef3bcbb94421a8bfa206d6e16
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 17:32:53 +0100

    Create login and logout test cases

--- src/core/data/infra/api-mock.ts
+++ src/core/data/infra/api-mock.ts
@@ -1,6 +1,6 @@
 import MockAdapter from 'axios-mock-adapter'
-import { REGISTER_URL } from '@data/gateways/api/constants'
-import { IFormSignUp } from '@domain/entities/formModels/signup-form.entity'
+import { LOGIN_URL, LOGOUT_URL, REGISTER_URL } from '@data/gateways/api/constants'
+import { IFormLogin, IFormSignUp } from '@domain/entities/formModels/signup-form.entity'
 
 export const mockAPIResponses = (
   axiosInstance: any, testError: boolean = false, baseDataRes: any = {}, pk: number | null = null
@@ -9,9 +9,43 @@ export const mockAPIResponses = (
   if (testError) {
     // User Registration
     mock.onPost(REGISTER_URL).reply(400, getUserRegistrationErrorResponse(baseDataRes))
+    // Login
+    mock.onPost(LOGIN_URL).reply(400, getUserLoginErrorResponse(baseDataRes))
+
   } else {
     // User Registration
     mock.onPost(REGISTER_URL).reply(201, formatUserCreateIntoResponse(baseDataRes))
+    // Login
+    mock.onPost(LOGIN_URL).reply(200, formatUserLoginIntoResponse(baseDataRes))
+    // Logout
+    mock.onPost(LOGOUT_URL).reply(200, formatUserLogoutIntoResponse())
+  }
+}
+
+/** Logout */
+const formatUserLogoutIntoResponse = () => {
+  return {
+    "detail": "Successfully logged out."
+  }
+}
+
+/** Login */
+const getUserLoginErrorResponse = (data: string) => {
+  return {
+    "non_form_errors": [data]
+  }
+}
+
+const formatUserLoginIntoResponse = (data: IFormLogin) => {
+  return {
+    "access": "xxx",
+    "refresh": "yyy",
+    "user": {
+      "pk": 5,
+      "first_name": "John",
+      "last_name": "Doe",
+      "username": data.username,
+    },
   }
 }
 
