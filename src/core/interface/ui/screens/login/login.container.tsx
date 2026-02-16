c795cad3bace0010e4a5b148dddec990369407ea
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 17:30:53 +0100

    Create login UI

---
+++ src/core/interface/ui/screens/login/login.container.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import LoginView from '@screens/login/login.view'
+import LoginController from '@screens/login/login.controller'
+import { IFormLogin } from '@domain/entities/formModels/signup-form.entity'
+
+
+export interface ILoginContainerViewModel {
+  children?: React.ReactNode
+}
+
+export const LoginContainer: React.FC<ILoginContainerViewModel> = (props) => {
+
+  const controller = new LoginController()
+  const handleSubmit = async (values: IFormLogin) => {
+     await controller.login(values)
+  };
+
+  return <LoginView
+    children={props.children}
+    handleSubmit={handleSubmit}
+  />
+}
