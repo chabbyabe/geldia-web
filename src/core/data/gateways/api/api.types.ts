d850edf9cc76bd175a71bed9b9712ddee5cb000c
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 16:29:19 +0100

    Initial API setup - Implements the connection between api form the back-end to fron-end

---
+++ src/core/data/gateways/api/api.types.ts
@@ -0,0 +1,47 @@
+/**
+ * Contains all the interfaces for the API responses
+ */
+
+
+export interface IBaseAPIModel {
+  id: number
+}
+
+export interface ApiDataResponseModel<T> {
+  success: boolean
+  code: number
+  error: string | null
+  response?: T
+}
+
+export interface IPagedAPIViewModel<T> {
+  previous: string | null
+  next: string | null
+  count: number
+  current_page_number: number
+  total_pages: number
+  results: T[]
+}
+
+export interface IErrorResponseModel {
+  detail?: string
+  error?: string
+  data?: any
+}
+
+export interface IUserModel extends IBaseAPIModel {
+  name: string
+  username: string
+  first_name: string
+  last_name: string
+}
+
+export interface IUserWithAccessTokenModel extends IBaseAPIModel {
+  access: string
+  refresh: string
+  user: IUserModel
+}
+
+export interface ILogoutResponseModel {
+  detail: string
+}
