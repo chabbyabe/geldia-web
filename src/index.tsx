ae872b8f6f21949227f3be8eb8d119b9a1a7f868
Abegail Isidro <chabbyabe@gmail.com>
2026-02-16 16:34:22 +0100

    Add API layer - Implements the API integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import './index.css'
+import reportWebVitals from '@base/reportWebVitals';
+import { store } from '@interface/presenters/store/store';
+import { App } from '@base/app';
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+import '@base/index.css'
 import 'react-toastify/dist/ReactToastify.css'
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
