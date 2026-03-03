21ea381ea63db51b970e8971176f20c22200b1b6
Abegail Isidro <chabbyabe@gmail.com>
2026-03-03 16:03:33 +0100

    Create transaction api interface and constants

--- src/core/data/gateways/api/api.types.ts
+++ src/core/data/gateways/api/api.types.ts
@@ -63,3 +63,64 @@ export interface IAccountModel extends IBaseAPIModel, ITimestampsModel {
   shared_users: IUserModel[],
   has_transactions: boolean
 }
+
+export interface ITransactionTypeModel extends IBaseAPIModel {
+  name: string
+  icon: string
+  color: string
+}
+
+export interface IAccountSimpleModel extends ITransactionTypeModel {
+  balance: number
+  is_default: boolean
+}
+
+export interface IStoreSimpleModel extends IBaseAPIModel {
+  name: string
+}
+
+export interface IPlaceSimpleModel extends IBaseAPIModel{
+  name: string
+}
+
+export interface ITagSimpleModel extends IBaseAPIModel{
+  name: string
+  color: string
+}
+
+export interface ICategorySimpleModel extends IBaseAPIModel, ITransactionTypeModel{
+  transaction_type: ITransactionTypeModel | null
+  parent_category: ICategorySimpleModel | null
+}
+
+export interface ITransactionModel extends IBaseAPIModel, ITimestampsModel {
+  name: string
+  user: IUserModel 
+  store: IStoreSimpleModel
+  place: IPlaceSimpleModel
+  account: IAccountSimpleModel
+  tags: ITagSimpleModel[]
+  transaction_type: ITransactionTypeModel 
+  amount: number
+  notes: string
+  net_amount: number
+  gross_amount: number
+  debit_month_year: string
+  external_transaction_id: number
+  pair_transaction: IAccountSimpleModel
+  is_recurring: boolean 
+  is_refunded: boolean
+  refunded_at: string
+  transaction_at: string
+  category: ICategorySimpleModel
+  recurring: boolean
+}
+
+export interface ITransactionFormInitialDataModel {
+  stores: IStoreSimpleModel[]
+  places: IPlaceSimpleModel[]
+  accounts: IAccountSimpleModel[]
+  categories: ICategorySimpleModel[]
+  transaction_types: ITransactionTypeModel[]
+  tags: ITagSimpleModel[]
+}
--- src/core/data/gateways/api/constants.ts
+++ src/core/data/gateways/api/constants.ts
@@ -10,3 +10,39 @@ export const REGISTER_URL = `/users/auth/register`
  */
 export const ACCOUNT_URL = `/ledger/accounts/`
 export const USER_URL = `/ledger/users/`
+export const TRANSACTION_URL = `/ledger/transactions/`
+
+
+/**
+ * PAGINATION CONSTANTS
+ */
+export const DEFAULT_TABLE_PAGE_SIZE = 20
+
+
+export const TRANSACTION_TYPE = {
+  INCOME: 1,
+  EXPENSES: 2,
+  TRANSFER: 3
+}
+
+export const USER_ACTIONS = {
+  CREATE: 1,
+  UPDATE: 2,
+  DELETE: 3
+}
+
+export const ACCOUNT_COLORS: string[] = [
+  "#006CD1",
+  "#0053A3",
+  "#4DA3FF",
+  "#2EB872",
+  "#F5A524",
+  "#E5484D"
+];
+
+export const DATE_RANGES = {
+  WEEK : "Week",
+  MONTH : "Month",
+  YEAR : "Year",
+  CUSTOM : "Custom"
+}
