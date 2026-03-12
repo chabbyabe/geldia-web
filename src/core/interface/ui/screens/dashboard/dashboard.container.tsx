fdae452f66f5aa983203d3f820958eebcd814e88
Abegail Isidro <chabbyabe@gmail.com>
2026-03-12 10:06:05 +0100

    Integrate summary overview into dashboard UI - Update dashboard container - Adjust controller logic - Update dashboard view

--- src/core/interface/ui/screens/dashboard/dashboard.container.tsx
+++ src/core/interface/ui/screens/dashboard/dashboard.container.tsx
@@ -1,11 +1,22 @@
+import { useAppSelector } from '@interface/presenters/store/hooks';
 import DashboardView from '@interface/ui/screens/dashboard/dashboard.view'
+import DashboardController from './dashboard.controller';
+import { useEffect } from 'react';
 
 export interface IDashboardContainerViewModel {
   children?: React.ReactNode
 }
 
 export const DashboardContainer: React.FC<IDashboardContainerViewModel> = (props) => {
+  const summaryCards = useAppSelector(state => state.dashboardState.summaryOverview);
+
+  useEffect(() => {
+    const controller = new DashboardController()
+    controller.retrieveSummaryOverview();
+  }, []);
+
   return <DashboardView
     children={props.children}
+    summaryOverview={summaryCards}
   />
 }
