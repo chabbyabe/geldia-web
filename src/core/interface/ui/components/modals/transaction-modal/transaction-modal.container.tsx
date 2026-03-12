0c6d7819e52dc39cda599954e96fbfd5b7b7149c
Abegail Isidro <chabbyabe@gmail.com>
2026-03-12 11:16:22 +0100

    Refactor transaction modal and screen components - Move transaction modal related functions from transaction container to transaction modal container to support re-usability of the transaction modal component without re-instantiating form values

--- src/core/interface/ui/components/modals/transaction-modal/transaction-modal.container.tsx
+++ src/core/interface/ui/components/modals/transaction-modal/transaction-modal.container.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IFormTransaction } from "@domain/entities/formModels/transaction-form.entity";
 import TransactionModalView from "./transaction-modal.view";
 import { ITransaction } from "@domain/entities/transaction/transaction.entity";
-import { ITransactionInitial } from "@domain/entities/transaction/initial.entity";
+import { useAppSelector } from "@interface/presenters/store/hooks";
+import TransactionModalController from "./transaction-modal.controller";
+import TransactionsController from "@interface/ui/screens/transactions/transactions.controller";
 
 interface ITransactionModalContainer {
   open: boolean
   onClose: () => void
-  formOptions: ITransactionInitial
-  handleFormSumbit: (values: IFormTransaction) => Promise<void> | void
   selectedTransaction: ITransaction | null
 }
 
 export const TransactionModalContainer: React.FC<ITransactionModalContainer> = (props) => {
+  const formOptions = useAppSelector(state => state.transactionState.options);
+  const currentUser = useAppSelector(state => state.authState.user);
+  const selectedTransaction = useAppSelector(state => state.transactionState.currentTransaction);
+  const transactionController = new TransactionsController();
+  const modalController = new TransactionModalController();
+
+  const handleFormSubmit = async (values: IFormTransaction) => {
+    const payload = {
+      ...values,
+      userId: currentUser?.id,
+      accountId: values.account,
+      transactionTypeId: values.transactionType,
+      categoryName: values.category,
+      storeName: values.store,
+      placeName:  values.place,
+      tagsNames: values.tags ?? [],
+    }
+    delete payload.category;
+    delete payload.store;
+    delete payload.place;
+
+    if (selectedTransaction) {
+      await transactionController.updateTransaction(selectedTransaction!.id, values)
+    }else{
+      await transactionController.createTransaction(payload);
+    }     
+    await modalController.retrieveFormOptions();
+  };
+
+  useEffect(() => {
+    modalController.retrieveFormOptions();
+  }, []);
+  
   return <TransactionModalView
     open={props.open}
     onClose={props.onClose}
-    formOptions={props.formOptions}
+    formOptions={formOptions}
     selectedTransaction={props.selectedTransaction}
-    handleFormSumbit={props.handleFormSumbit}
+    handleFormSumbit={handleFormSubmit}
   />;
 };
