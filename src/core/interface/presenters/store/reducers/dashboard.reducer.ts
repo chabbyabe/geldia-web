e13e03d73e2e69acb2a957e41a02411e7bc5ccfc
Abegail Isidro <chabbyabe@gmail.com>
2026-03-12 11:10:54 +0100

    Create recent transaction state holder

--- src/core/interface/presenters/store/reducers/dashboard.reducer.ts
+++ src/core/interface/presenters/store/reducers/dashboard.reducer.ts
@@ -1,12 +1,15 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { ISummary } from "@base/core/domain/entities/dashboard/summary-overview.entity";
+import { ISummary } from "@domain/entities/dashboard/summary-overview.entity";
+import { ITransaction } from "@domain/entities/transaction/transaction.entity";
 
 interface IDashboardState {
-  summaryOverview: ISummary[],
+  summaryOverview: ISummary[]
+  recentTransactions: ITransaction[]
 }
 
 const initialState: IDashboardState = {
   summaryOverview: [],
+  recentTransactions: []
 }
 
 export const dashboardSlice = createSlice({
@@ -16,10 +19,14 @@ export const dashboardSlice = createSlice({
     retrieveSummaryOverview(state, action: PayloadAction<ISummary[]>) {
       state.summaryOverview = [...action.payload]
     },
+     retrieveRecentTransactions(state, action: PayloadAction<ITransaction[]>) {
+      state.recentTransactions = [...action.payload]
+    },
   },
 })
 
 export const {
   retrieveSummaryOverview,
+  retrieveRecentTransactions
 } = dashboardSlice.actions
 export default dashboardSlice.reducer
