8f0f7d753ac92fa0d6e35bf8cd2dc92d2a58e1b6
Abegail Isidro <chabbyabe@gmail.com>
2026-03-12 10:06:05 +0100

    Integrate summary overview into dashboard UI - Update dashboard container - Adjust controller logic - Update dashboard view

--- src/core/interface/ui/screens/dashboard/dashboard.controller.ts
+++ src/core/interface/ui/screens/dashboard/dashboard.controller.ts
@@ -1,3 +1,18 @@
-export default class DashboardController {
+import DashboardApiGateway from "@data/gateways/api/services/dashboard.gateway"
+import DashboardRepository from "@data/gateways/api/services/dashboard.repository"
+import RetrieveSummaryOverviewUseCase from "@domain/usecases/dashboard/retrieve-summary-overview.usecase"
 
+export default class DashboardController {
+  private readonly retrieveSummaryOverviewUseCase: RetrieveSummaryOverviewUseCase
+
+  constructor() {
+    this.retrieveSummaryOverviewUseCase = new RetrieveSummaryOverviewUseCase(
+      new DashboardApiGateway(),
+      new DashboardRepository()
+    )
+  }
+
+  async retrieveSummaryOverview() {
+    return await this.retrieveSummaryOverviewUseCase.execute()
+  }
 }
