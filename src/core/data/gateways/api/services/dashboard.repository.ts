07495095201abd03b88b5bbc54b962f338eb5b5a
Abegail Isidro <chabbyabe@gmail.com>
2026-03-12 11:13:53 +0100

    Implement recent transaction retrieval in dashboard repository

--- src/core/data/gateways/api/services/dashboard.repository.ts
+++ src/core/data/gateways/api/services/dashboard.repository.ts
@@ -1,9 +1,13 @@
+import { ITransaction } from "@domain/entities/transaction/transaction.entity"
 import { ISummary } from "@domain/entities/dashboard/summary-overview.entity"
-import { retrieveSummaryOverview } from "@interface/presenters/store/reducers/dashboard.reducer"
+import { retrieveSummaryOverview, retrieveRecentTransactions } from "@interface/presenters/store/reducers/dashboard.reducer"
 import { store } from "@interface/presenters/store/store"
 
 export default class DashboardRepository {
   retrieveSummaryOverview(summaries: ISummary[]) {
     store.dispatch(retrieveSummaryOverview(summaries))
+  }
+  retrieveRecentTransactions(transactions: ITransaction[]) {
+    store.dispatch(retrieveRecentTransactions(transactions))
   }
 }
