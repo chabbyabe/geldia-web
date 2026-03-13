45fa8e166fbdaf0e4168eedd9a7499b6b64f0987
Abegail Isidro <chabbyabe@gmail.com>
2026-03-13 16:35:32 +0100

    Integrate date filter component to category overview component

--- src/core/interface/ui/components/dashboard/category-overview/category-overview.container.tsx
+++ src/core/interface/ui/components/dashboard/category-overview/category-overview.container.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { ICategoryOverview } from "@domain/entities/dashboard/category-overview.entity";
 import CategoryOverviewView from "./category-overview.view";
+import { useAppSelector } from "@interface/presenters/store/hooks";
+import CategoryOverviewController from "./category-overview.controller";
 
 interface ICategoryOverviewContainer {
-  data: ICategoryOverview[];
+  children?: React.ReactNode;
 }
 
 export const CategoryOverviewContainer: React.FC<ICategoryOverviewContainer> = (props) => {
+  const { categoryOverview, categoryOverviewFilterParams } = useAppSelector(
+    state => ({
+      categoryOverview: state.dashboardState.categoryOverview,
+      categoryOverviewFilterParams: state.dashboardState.filters?.categoryOverview
+    })
+  );
+  
+  const controller = new CategoryOverviewController()
+
   return <CategoryOverviewView
-    data={props.data}
+    data={categoryOverview}
+    onFilterChange={controller.retrieveCategoryOverview.bind(controller)}
+    filterParams={categoryOverviewFilterParams}
   />;
 };
