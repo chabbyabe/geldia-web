59addccc38e1f9c1afe9e324d592fb3c4622cc04
Abegail Isidro <chabbyabe@gmail.com>
2026-03-16 13:20:40 +0100

    Create year overview mapper and entities

--- src/core/data/gateways/api/services/mappers/dashboard.mappers.ts
+++ src/core/data/gateways/api/services/mappers/dashboard.mappers.ts
@@ -1,9 +1,10 @@
 import { ISummary } from '@base/core/domain/entities/dashboard/summary-overview.entity';
 import { ITransaction } from '@base/core/domain/entities/transaction/transaction.entity';
-import { ICategoryOverviewModel, ISummaryModel, ITransactionModel } from '@data/gateways/api/api.types'
+import { ICategoryOverviewModel, ISummaryModel, ITransactionModel, IYearOverviewModel } from '@data/gateways/api/api.types'
 import { objectToCamel } from 'ts-case-convert';
 import { mapTransactionAttributes } from './transaction.mappers';
 import { ICategoryOverview } from '@domain/entities/dashboard/category-overview.entity';
+import { IYearOverview } from '@domain/entities/dashboard/year-overview.entity';
 
 export const mapSummaryOverviewAttributes = (initialModel: ISummaryModel[]): ISummary[] => {
   return [...objectToCamel(initialModel)];
@@ -17,5 +18,9 @@ export const mapRecentTransactionAttributes = (initialModel: ITransactionModel[]
 }
 
 export const mapCategoryOverviewAttributes = (initialModel: ICategoryOverviewModel[]): ICategoryOverview[] => {
+  return [...objectToCamel(initialModel)];
+}
+
+export const mapYearOverviewAttributes = (initialModel: IYearOverviewModel[]): IYearOverview[] => {
   return [...objectToCamel(initialModel)];
 }
